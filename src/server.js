807c1177c3f1bba7b9bd6dbdcefd3a2f125dc9b8
DongWon Sehr <dongwon@its-newid.com>
2021-12-28 07:23:17 +0900

    set frontend

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug"); // configure view engine as pug
+app.set("views", __dirname + "/views"); // tell to express where is template
+app.use("/public", express.static(__dirname + "/public")); // create public url for user
+app.get("/", (req,res) => res.render("home")); // route handler
 
-app.listen("3000");
+const handleListen = () => console.log(`Listening to http://localhost:3001`);
+app.listen("3001", handleListen);
