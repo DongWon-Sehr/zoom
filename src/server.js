3d276555863b3a2f3e48dccbfa485575a163f974
DongWon Sehr <dongwon@its-newid.com>
2021-12-28 15:52:47 +0900

    redirect to home

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const app = express();
 app.set("view engine", "pug"); // configure view engine as pug
 app.set("views", __dirname + "/views"); // tell to express where is template
 app.use("/public", express.static(__dirname + "/public")); // create public url for user
-app.get("/", (req,res) => res.render("home")); // route handler
+app.get("/", (_,res) => res.render("home")); // route handler
+app.get("/*", (_,res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening to http://localhost:3001`);
-app.listen("3001", handleListen);
+const handleListen = () => console.log(`Listening to http://localhost:3000`);
+app.listen("3000", handleListen);
